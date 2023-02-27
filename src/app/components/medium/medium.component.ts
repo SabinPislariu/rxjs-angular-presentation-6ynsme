49ebc92648ca9f97e05f6a54397a413bd0d73983
SabinPislariu <53428660+SabinPislariu@users.noreply.github.com>
2023-02-27 21:18:49 +0100

    1 file modified

--- src/app/components/medium/medium.component.ts
+++ src/app/components/medium/medium.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Age, User } from '../../user.model';
+import { Age, AgeType, User } from '../../user.model';
 import { UserService } from '../../user.service';
 import { shareReplay, combineLatest, Observable, take } from 'rxjs';
 
@@ -10,28 +10,39 @@ import { shareReplay, combineLatest, Observable, take } from 'rxjs';
 })
 export class MediumComponent implements OnInit {
   public users: User[] = [];
+  public ageTypes: AgeType[] = [];
   constructor(private readonly _userService: UserService) {}
 
   ngOnInit() {
     combineLatest([
       this._userService.getUsers(),
       this._userService.getBlackListUsers(),
+      this._userService.getAgeType(),
     ])
       .pipe(shareReplay(1), take(1))
-      .subscribe(([users, blacklistId]) =>
+      .subscribe(([users, blacklistId, ageTypes]) => {
+        this.ageTypes = ageTypes;
         users.forEach((user) =>
           blacklistId.forEach((blacklistId) => {
             if (user.id == blacklistId.id) {
               this.users.push(user);
             }
           })
-        )
-      );
+        );
+      });
   }
 
   public showAlert(id: number) {
-    let age: Age;
-    this._userService.getAgeById(id).subscribe((ageObj) => (age = ageObj));
-    alert(age.age);
+    this._userService
+      .getAgeById(id)
+      .pipe(take(1))
+      .subscribe((age) => {
+        const ageType = this.ageTypes.find(
+          (ageType) =>
+            parseInt(ageType.ageMax) >= parseInt(age.age) &&
+            parseInt(ageType.ageMin) <= parseInt(age.age)
+        );
+        alert('age ' + age.age + ' Age Type ' + ageType.type);
+      });
   }
 }
