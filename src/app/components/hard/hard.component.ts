7be428f787badd75f5d43ae1d8e5033b19381cf9
SabinPislariu <53428660+SabinPislariu@users.noreply.github.com>
2023-02-27 22:06:58 +0100

    2 files modified

--- src/app/components/hard/hard.component.ts
+++ src/app/components/hard/hard.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { first, shareReplay } from 'rxjs';
+import { UserAge, UserId } from '../../user.model';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-hard',
   templateUrl: './hard.component.html',
-  styleUrls: ['./hard.component.css']
+  styleUrls: ['./hard.component.css'],
 })
 export class HardComponent implements OnInit {
-
-  constructor() { }
+  public user: UserAge;
+  private _blackList: UserId[] = [];
+  constructor(private readonly _userService: UserService) {}
 
   ngOnInit() {
+    this._userService
+      .getBlackListUsers()
+      .pipe(shareReplay(1), first())
+      .subscribe((blackListUsers) => (this._blackList = blackListUsers));
   }
 
+  public validateForm() {
+    let x = document.forms['myForm']['fname'].value;
+    if (x < 0 || x > 40) {
+      document.getElementById('alrt').innerHTML =
+        '<b>Select Another Number</b>';
+      setTimeout(function () {
+        document.getElementById('alrt').innerHTML = '';
+      }, 10000);
+    } else if (isNaN(x) || x == '') {
+      document.getElementById('alrt').innerHTML =
+        '<b>Please insert a number</b>';
+      setTimeout(function () {
+        document.getElementById('alrt').innerHTML = '';
+      }, 10000);
+    } else {
+    }
+  }
 }
+
